e554f31a11cb55b406b0aa5371a87c4c01fcb12b
ArseneJuzo <arsenendongoruphin@gmail.com>
2025-01-28 20:28:48 +0100

    commit 1

---
+++ ABC.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ABC {
+    public static void main(String[] args){
+      Scanner input = new Scanner(System.in);
+      String[] nom={"ARSENE","baltimore","claire","dom","ephaistos","ferran","garp","hermes","iron","jon","kall","lamine","map","naruto","oppote","patrick","q","ren","seiko","takakura","ulquira","vald","waly","xen","yelena","zed"};
+        System.out.println("entrer");
+      String m=input.next();
+      for (String l:nom){
+          if (m.toLowerCase().equals(l.substring(0,1))){
+              System.out.println(l);
+              if (m.toLowerCase().equals("a")){
+                  System.out.print("le BG");
+
+
+              }
+          }
+      }
+    }
+}
